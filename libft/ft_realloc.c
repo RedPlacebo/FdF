d3dbca3ba8e703c71173ad54b0914727d78fa053
RedPlacebo <ikarishev@gmail.com>
2017-12-20 13:31:36 -0800

    Add files via upload
    _updated libft

--- libft/ft_realloc.c
+++ libft/ft_realloc.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ikarishe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/05 16:13:35 by ikarishe          #+#    #+#             */
+/*   Updated: 2017/12/05 16:14:42 by ikarishe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char	*ft_realloc(char *str, int size)
 {
-	char *temp;
-	int i;
-	
+	char	*temp;
+	int		i;
+
 	temp = str;
 	str = (char *)malloc(sizeof(char) * size);
 	i = 0;
@@ -15,4 +27,4 @@ char	*ft_realloc(char *str, int size)
 	}
 	free(temp);
 	return (str);
-}	
+}
--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,72 +6,76 @@
 /*   By: ikarishe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/16 12:29:30 by ikarishe          #+#    #+#             */
-/*   Updated: 2017/10/31 12:59:48 by ikarishe         ###   ########.fr       */
+/*   Updated: 2017/12/04 16:33:54 by ikarishe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/* check that the file directory, and **line are valid
- * check that there's stuff to read in the file
- * 
- * Initialize a buffer
- * Keep reading BUFF_SIZE characters into temp
- * 		until you hit a \n or read 0/-1 bytes
- * use strjoin to stick what you read onto the end
- * the buffer. strjoin auto-mallocs.
- *
- * now that buffer either has a \n or doesn't,
- * 		if it has a \n
- * 		find where that \n is, and overwrite it with a \0
- * 		now we can strdup it into *line. Its still holding characters
- * 		so make the buffer pointer point to where that \n was + 1
- * 		exit;
- * if it doesn't have a \n
- *		you're at the end of the file. The buffer might hold stuff if the
- *		file doesn't end in a \n. Dump whats in the buffer into *line.
- *		clear the buffer.
- *		exit;
- * */
 
 #include "libft.h"
 #include "get_next_line.h"
 
-static int	read_tmp(const int fd, char **buf)
+static t_list	*get_fd(t_list **file, int fd)
 {
-	char 	str1[BUFF_SIZE + 1];
-	int		bytes_read;
+	t_list	*tmp;
 
-	bytes_read = 1;
-	while (bytes_read > 0 && ft_strchr(*buf, '\n') == NULL)
+	tmp = *file;
+	while (tmp)
 	{
-		bytes_read = read(fd, &str1, BUFF_SIZE);
-		if (bytes_read < 0)
-			return (-1);
-		str1[bytes_read] = '\0';
-		*buf = ft_strjoin(*buf, (const char *)str1);
-		ft_bzero(str1, BUFF_SIZE);
+		if ((int)tmp->content_size == fd)
+			return (tmp);
+		tmp = tmp->next;
 	}
-	return (bytes_read);
+	tmp = ft_lstnew("\0", fd);
+	ft_lstadd(file, tmp);
+	tmp = *file;
+	return (tmp);
 }
 
-int		get_next_line(const int fd, char **line)
+static void		read_tmp(const int fd, void **main_buffer)
 {
-	static char 	*buffer;
-	char	*str;
-	int 	len;
+	char	sub_buffer[BUFF_SIZE + 1];
+	int		bytes_read;
+	char	*where_i_was_pointing;
 
-	if (fd < 0 || line == NULL || (-1 == read(fd, buffer, 0)))
-		return (-1);
-	if (!buffer)
-		buffer = ft_strnew(BUFF_SIZE);
-	len = read_tmp(fd, &buffer);
-	if ((str = ft_strchr(buffer, '\n')) != NULL)
+	bytes_read = 1;
+	while (bytes_read > 0 && ft_strchr(*main_buffer, '\n') == NULL)
 	{
-		str[0] = '\0';
-		*line = ft_strdup(buffer);
-		buffer = str + 1;
+		bytes_read = read(fd, &sub_buffer, BUFF_SIZE);
+		if (bytes_read < 0)
+			return ;
+		sub_buffer[bytes_read] = '\0';
+		where_i_was_pointing = *main_buffer;
+		*main_buffer = ft_strjoin(*main_buffer, (const char *)sub_buffer);
+		free(where_i_was_pointing);
+		ft_bzero(sub_buffer, BUFF_SIZE);
+	}
+	return ;
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	char			*cut_ptr;
+	char			*tmp;
+	static t_list	*file;
+	t_list			*curr;
+
+	if (fd < 0 || line == NULL || (-1 == read(fd, NULL, 0)))
+		return (-1);
+	curr = get_fd(&file, fd);
+	if (!curr->content)
+		curr->content = ft_strnew(BUFF_SIZE);
+	read_tmp(curr->content_size, &curr->content);
+	if ((cut_ptr = ft_strchr(curr->content, '\n')) != NULL)
+	{
+		*cut_ptr = '\0';
+		*line = ft_strdup(curr->content);
+		tmp = ft_strdup(cut_ptr + 1);
+		free(curr->content);
+		curr->content = ft_strdup(tmp);
+		free(tmp);
 		return (1);
 	}
-	*line = ft_strdup(buffer);
-	buffer = NULL;	
+	*line = ft_strdup(curr->content);
+	free(curr->content);
+	curr->content = NULL;
 	return (ft_strlen(*line) > 0 ? 1 : 0);
 }
